e3b8ce7471f01c202ab49fbda5feca708175714c
Ankit Gaurkar <93152878+AnkitGaurkar@users.noreply.github.com>
2022-03-14 20:23:13 +0530

    Example of front and back end data fetching....

---
+++ Student.java
@@ -0,0 +1,58 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Student
+ */
+@WebServlet("/Student")
+public class Student extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Student() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		
+		String sid=request.getParameter("sid");
+		String sname=request.getParameter("sname");
+		String saddress=request.getParameter("saddress");
+		String course[]=request.getParameterValues("course");
+		
+		pw.print(sid+"\t"+sname+"\t"+saddress);
+		
+		for(String c1:course)
+		{
+			pw.print("<br>"+c1);
+		}
+	}
+
+}
