eb87edb6bb0fd7461949ff55c239bc124fb855a1
Ankit Gaurkar <93152878+AnkitGaurkar@users.noreply.github.com>
2022-03-15 17:56:57 +0530

    Servlet With JDBC

--- Student.java
+++ Student.java
@@ -1,4 +1,5 @@
 package com;
+import java.sql.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -47,11 +48,33 @@ public class Student extends HttpServlet {
 		String saddress=request.getParameter("saddress");
 		String course[]=request.getParameterValues("course");
 		
-		pw.print(sid+"\t"+sname+"\t"+saddress);
+		pw.print(sid+" "+sname+" "+saddress);
 		
+		String courselist="";
 		for(String c1:course)
 		{
+			courselist=courselist+" "+c1;
+			
 			pw.print("<br>"+c1);
+		}
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/servletadjt","root","root");//Connection Established
+			String sql="insert into student values(?,?,?,?)";
+			PreparedStatement pst=con.prepareStatement(sql);
+			pst.setInt(1, Integer.parseInt(sid));
+			pst.setString(2, sname);
+			pst.setString(3, saddress);
+			pst.setString(4, courselist);
+			
+			
+			pst.execute();
+			
+			pw.print(" Record Inserted");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
